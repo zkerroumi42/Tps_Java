065733d20720a5a614641e6fe30fb8226c26b7f7
zkerroumi42 <za.kerroumi42@gmail.com>
2023-11-02 22:00:10 +0100

    finish 1ere travail en interface graphique java

--- interfaces_graphic/Gestion.java
+++ interfaces_graphic/Gestion.java
@@ -1,8 +1,15 @@
 package interfaces_graphic;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
-public class Gestion  {
+public class Gestion implements ActionListener {
     Frame fen;
     Label lblnom;
     Label lblprenom;
@@ -27,10 +34,12 @@ public class Gestion  {
     Checkbox m;
     Checkbox d;
     TextArea area;
-    Button file;
-    Label showurl;
-    Button effacer;
-    Button enregistrer;
+    Button btnfile;
+    Label lblshowurl;
+    Button btneffacer;
+    Button btnenregistrer;
+
+    String photourl;
 
 
 
@@ -127,20 +136,23 @@ public class Gestion  {
         lblphoto=new Label("Photo:");
         lblphoto.setBounds(x,y*8+hl+hl*3,ll,hl);
         fen.add(lblphoto);
-        file=new Button("Choose File");
-        file.setBounds(x+ll+5,y*8+hl+hl*3,ll/2,hl);
-        fen.add(file);
-        showurl=new Label(" ");
-        showurl.setBounds(x+ll+ll/2,y*8+hl+hl*3,ll/2,hl);
+        btnfile=new Button("Choose File");
+        btnfile.setBounds(x+ll+5,y*8+hl+hl*3,ll/2,hl);
+        btnfile.addActionListener(this);
+        fen.add(btnfile);
+        lblshowurl=new Label(" ");
+        lblshowurl.setBounds(x+ll+ll/2,y*8+hl+hl*3,ll/2,hl);
 
 
-        effacer=new Button("Effacr");
-        effacer.setBounds(x,y*10+hl+hl*3,ll/2,hl);
-        fen.add(effacer);
-        enregistrer=new Button("Enregistrer");
-        enregistrer.setBounds(x+ll+5,y*10+hl+hl*3,ll/2,hl);
-        fen.add(enregistrer);
+        btneffacer=new Button("Effacr");
+        btneffacer.setBounds(x,y*10+hl+hl*3,ll/2,hl);
+        fen.add(btneffacer);
+        btneffacer.addActionListener(this);
 
+        btnenregistrer=new Button("Enregistrer");
+        btnenregistrer.setBounds(x+ll+5,y*10+hl+hl*3,ll/2,hl);
+        fen.add(btnenregistrer);
+        btnenregistrer.addActionListener(this);
 
 
 
@@ -156,5 +168,97 @@ public class Gestion  {
     }
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==btnfile) {
+            Dialog filechoose = new FileDialog(fen, "Choose a File", FileDialog.LOAD);
+            filechoose.setVisible(true);
+            String selectedDirectory = ((FileDialog) filechoose).getDirectory();
+            String selectedFile = ((FileDialog) filechoose).getFile();
+
+            if (selectedFile != null) {
+                System.out.println("Selected Directory: " + selectedDirectory);
+                System.out.println("Selected File: " + selectedFile);
+                photourl = selectedFile;
+                lblshowurl.setText(photourl);
+            }
+            fen.add(lblshowurl);
+            photourl=lblshowurl.getText();
+
+        }
+
+
+
+        fen.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+
+        if(e.getSource()==btneffacer){
+            txtnom.setText("");
+            txtprenom.setText("");
+            txtmail.setText("");
+            txtnaissance.setText("");
+            txtpass.setText("");
+            boxgroup.setSelectedCheckbox(boxH);
+            l.setState(false);
+            m.setState(false);
+            d.setState(false);
+
+            choice.select(0);
+            lblshowurl.setText("");
+            area.setText("");
+
+
+        }
+        if(e.getSource()==btnenregistrer){
+            String nom=txtnom.getText()+"\n";
+            String prenom=txtprenom.getText()+"\n";
+            String date=txtnaissance.getText()+"\n";
+            String email=txtmail.getText()+"\n";
+            String pass=txtpass.getText()+"\n";
+            String genre= (boxgroup.getSelectedCheckbox().getLabel())+"\n";
+            String pay=choice.getSelectedItem()+"\n";
+            String diplome = "";
+            if(l.getState()==true){
+                diplome+=l.getLabel()+"\n";
+            }
+            if(m.getState()==true){
+                diplome+=m.getLabel()+"\n";
+            }
+            if(d.getState()==true){
+                diplome+=d.getLabel()+"\n";
+            }
+            String comment=area.getText()+"\n";
+
+
+            try {
+                FileOutputStream out = new FileOutputStream("zakaria.txt");
+                out.write(nom.getBytes());
+                out.write(prenom.getBytes());
+                out.write(date.getBytes());
+                out.write(email.getBytes());
+                out.write(pass.getBytes());
+                out.write(genre.getBytes());
+                out.write(pay.getBytes());
+                out.write(diplome.getBytes());
+                out.write(comment.getBytes());
+                if (photourl != null) {
+                    out.write(photourl.getBytes());
+
+                }
+                out.close();
+            } catch (FileNotFoundException ex) {
+                throw new RuntimeException(ex);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+
+        }
+
+
+    }
 }
 
